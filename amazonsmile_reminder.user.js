06d1d59d82610f5e81a4a1406f6eb033e00d9f2c
Raymond Chee <rjchee@gmail.com>
2018-02-04 00:21:54 -0600

    Add Amazon Smile reminder userscript

---
+++ amazonsmile_reminder.user.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name         AmazonSmile Reminder
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Reminds you to switch to AmazonSmile
+// @author       Raymond Chee
+// @match        https://www.amazon.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    var navBar = document.getElementsByClassName("nav-right");
+    if (navBar) {
+        var smileDiv = document.createElement("div");
+        smileDiv.style.cssText = "margin-top: 10px; margin-bottom: 10px; line-height: 40px;";
+        var smileSpan = document.createElement("span");
+        smileSpan.textContent = "Don't forget to purchase from ";
+        smileSpan.style.cssText = "vertical-align: middle; color: #ccc";
+        smileDiv.appendChild(smileSpan);
+        var smileLink = document.createElement("a");
+        smileLink.href = new URL(window.location.href);
+        smileLink.hostname = "smile.amazon.com";
+        var image = document.createElement("img");
+        image.src = "https://github.com/rjchee/my-userscripts/raw/master/Amazon_Smile_logo.png";
+        image.height = 40;
+        smileLink.appendChild(image);
+        smileDiv.appendChild(smileLink);
+        navBar[0].appendChild(smileDiv);
+    }
+})();
