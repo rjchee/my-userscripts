b33c4c58cfad178e8002ab51948c8863dcd8c9e5
Raymond Chee <rjchee@gmail.com>
2018-04-06 14:19:10 -0500

    Warn user when pasting zero-width characters

--- detect-zero-width-clipboard-data.user.js
+++ detect-zero-width-clipboard-data.user.js
@@ -1,12 +1,11 @@
 // ==UserScript==
 // @name         Detect Zero-Width characters when copying text
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  This script alerts you if you copy a zero-width character and allows you to remove them to prevent being fingerprinted.
 // @author       Raymond Chee
 // @match        *://*/*
 // @grant        none
-// @noframes
 // ==/UserScript==
 
 (function() {
@@ -18,7 +17,7 @@
         return zeroWidthChars.test(s);
     }
     function replaceZeroWidth(s) {
-        return s.split(zeroWidthChars).join('\u2022');
+        return s.split(zeroWidthChars).join('•');
     }
     function removeZeroWidth(s) {
         return s.split(zeroWidthChars).join('');
@@ -27,14 +26,32 @@
         var selection = window.getSelection();
         var data = selection.toString();
         if (containsZeroWidth(data)) {
-            var msg = "Your " + (e.type === "copy" ? "copied" : "cut" ) + " selection contains zero-width characters! Do you want to remove them? Here are the locations of those characters:\n\n";
-            msg += replaceZeroWidth(data);
+            var msg =
+                `Your ${e.type === "copy" ? "copied" : "cut"} selection contains zero-width characters! Here are the locations of those characters:
+
+${replaceZeroWidth(data)}
+
+Do you want to remove them?`;
             if (window.confirm(msg)) {
                 e.preventDefault();
                 e.clipboardData.setData('text/plain', removeZeroWidth(data));
             }
         }
     }
+    function handlePasteEvent(e) {
+        var data = e.clipboardData.getData('text');
+        if (containsZeroWidth(data)) {
+            var msg = `Your pasted text contains zero-width characters! Here are the locations of those characters:
+
+${replaceZeroWidth(data)}
+
+If you want to fix it, here is your pasted text without the zero-width characters (you will have to copy and paste it again):
+
+${removeZeroWidth(data)}`;
+            window.alert(msg);
+        }
+    }
     document.addEventListener('copy', handleCopyEvent);
     document.addEventListener('cut', handleCopyEvent);
+    document.addEventListener('paste', handlePasteEvent);
 })();
