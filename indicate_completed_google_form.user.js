5c03439b481f3985f821c0b6c34dbe9ae50e7444
Raymond Chee <rjchee@gmail.com>
2018-09-15 22:27:47 -0400

    Fix bug in multi-part google forms

--- indicate_completed_google_form.user.js
+++ indicate_completed_google_form.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Have I Filled out This Form Before?
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Script to mark whether you've filled out a Google Form before
 // @author       Raymond Chee
 // @match        https://docs.google.com/forms/d/e/*/*
@@ -14,30 +14,25 @@
     // Your code here...
     var url = new URL(window.location);
     var path = url.pathname.split("/");
-    console.log(path);
     var id = "filled-"+path[4];
     var stored = localStorage.getItem(id);
-    var page = path[5];
     var indicator = document.createElement("div");
-    switch (page) {
-        case "viewform":
-            var titleBox = document.getElementsByClassName("freebirdFormviewerViewHeaderTitleRow")[0];
-            if (stored === null) {
-                indicator.textContent = "(0)";
-                indicator.style.color = "red";
-            } else {
-                indicator.textContent = "(" + stored + ")";
-                indicator.style.color = "green";
-            }
-            titleBox.appendChild(indicator);
-            break;
-        case "formResponse":
-            var val = (stored === null ? 0 : parseInt(stored, 10)) + 1;
-            localStorage.setItem(id, val);
-            var pageTitle = document.getElementsByClassName("freebirdFormviewerViewResponsePageTitle")[0];
-            indicator.textContent = "(" + val + ")";
+    var titleBox = document.getElementsByClassName("freebirdFormviewerViewHeaderTitleRow")[0];
+    if (titleBox !== undefined) {
+        if (stored === null) {
+            indicator.textContent = "(0)";
+            indicator.style.color = "red";
+        } else {
+            indicator.textContent = "(" + stored + ")";
             indicator.style.color = "green";
-            pageTitle.appendChild(indicator);
-            break;
+        }
+        titleBox.appendChild(indicator);
+    } else {
+        var val = (stored === null ? 0 : parseInt(stored, 10)) + 1;
+        localStorage.setItem(id, val);
+        var pageTitle = document.getElementsByClassName("freebirdFormviewerViewResponsePageTitle")[0];
+        indicator.textContent = "(" + val + ")";
+        indicator.style.color = "green";
+        pageTitle.appendChild(indicator);
     }
 })();
